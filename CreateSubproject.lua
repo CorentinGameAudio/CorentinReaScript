cc7471cab4e7befdc34eb3706a5d165ff9a912a3
corentin.bertiaux <corentin@g4f.fr>
2025-10-07 15:43:51 +0200

    Add subproject scripts

---
+++ CreateSubproject.lua
@@ -0,0 +1,58 @@
+-- @description Create subproject copying first track item in time selection
+-- @author corentinB
+-- @version 1.0
+-- @about
+
+
+function CreateTracks(trackColor)
+ for i = 1, 15 do
+    reaper.InsertTrackAtIndex(i, false)
+    local track = reaper.GetTrack(0, i)
+    reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "SFX", true)
+    reaper.SetTrackColor(track, trackColor)
+  end
+end
+  
+  
+--MAIN --
+mainProjTrack = reaper.GetSelectedTrack(0, 0)
+color = 0
+if mainProjTrack then
+  color = reaper.GetTrackColor(mainProjTrack)
+end
+
+-- create subproject
+reaper.Main_OnCommand(41049, 0) 
+
+-- unselect items and tracks
+actionNumber = reaper.NamedCommandLookup("_SWS_UNSELALL")
+reaper.Main_OnCommand(actionNumber, 0) 
+
+-- select first track
+actionNumber = reaper.NamedCommandLookup("_SWS_SEL1")
+reaper.Main_OnCommand(actionNumber, 0)
+
+-- select item in time selection on selected track
+reaper.Main_OnCommand(40718, 0)
+
+-- copy
+reaper.Main_OnCommand(41383, 0)
+
+-- switch last tab
+actionNumber = reaper.NamedCommandLookup("_SWS_LASTPROJTAB")
+reaper.Main_OnCommand(actionNumber, 0)
+
+-- paste
+reaper.Main_OnCommand(42398, 0)
+
+-- create some default tracks
+CreateTracks(color)
+
+-- rename first track in video
+track = reaper.GetTrack(0, 0)
+reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "video", true)
+reaper.SetTrackUIVolume(track, -150, false, true, 1)
+
+-- make master track visible
+reaper.SetMasterTrackVisibility(1)
+
---
+++ SplitSubprojectRegionIntoTakes.lua
@@ -0,0 +1,88 @@
+-- @description Split Subproject Regions Into Takes
+-- @author corentinB
+-- @version 1.0
+-- @about
+
+
+function IsSubProject(item)
+    return select(2, reaper.GetItemStateChunk(item, '', false)):find 'SOURCE RPP_PROJECT' and true or false
+end
+
+function GetRegionsByIDX(projectIndex)
+  local i=0
+  local regions_idx = {}
+  local startPos = 0
+  repeat
+    local iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(projectIndex,i)
+    if iRetval >= 1 then
+      if bIsrgnOut == true then -- NOTE: Mod
+        local region = {}
+        region.pos_start = iPosOut
+        region.pos_end = iRgnendOut
+        region.color = iColorOur -- In case field is only $blank to clear
+        region.name = sNameOut
+        region.idx = iMarkrgnindexnumberOut
+        regions_idx[iMarkrgnindexnumberOut] = region
+      else
+        if sNameOut == "=START" then
+          startPos = iPosOut
+        end
+      end
+    end
+    i = i+1
+  until iRetval == 0
+  return regions_idx, startPos
+end
+
+
+-- MAIN --
+regions = {}
+nbItem = reaper.CountSelectedMediaItems(0)
+selectedItem = {}
+copiedItem = {}
+startPosition = 0
+
+-- Get select item
+for i = 1, nbItem do
+  local item = reaper.GetSelectedMediaItem(0, i-1)
+  selectedItem[i] = item
+end
+
+
+-- For each item, add take with the same source and shift the start
+-- Resize the item to the max region size
+for itemIndex = 1, #selectedItem do
+  if IsSubProject(selectedItem[itemIndex]) then
+    local itemPosition = reaper.GetMediaItemInfo_Value(selectedItem[itemIndex], "D_POSITION")
+    local take =  reaper.GetActiveTake(selectedItem[itemIndex])
+    local takeSource = reaper.GetMediaItemTake_Source(take)
+    local maxRegionLength = 0
+    
+    if take then 
+      local src = reaper.GetMediaItemTake_Source(take)
+      subproj = reaper.GetSubProjectFromSource(src)
+      regions, startPosition = GetRegionsByIDX(subproj)
+      reaper.SetMediaItemTakeInfo_Value(take, "D_STARTOFFS", regions[1].pos_start - startPosition)
+      
+      if #regions > 1 then
+        for regionIndex = 2, #regions do
+          local regionLength = regions[regionIndex].pos_end - regions[regionIndex].pos_start
+          if regionLength > maxRegionLength then
+            maxRegionLength = regionLength
+          end
+          
+          local newTake = reaper.AddTakeToMediaItem(selectedItem[itemIndex])
+          reaper.SetMediaItemTake_Source(newTake, takeSource)
+          reaper.SetMediaItemTakeInfo_Value(newTake, "D_STARTOFFS", regions[regionIndex].pos_start - startPosition)
+        end
+      end
+      
+      reaper.SetMediaItemLength(selectedItem[itemIndex], maxRegionLength, true)
+    end
+  end
+end
+
+-- select random takes
+actionNumber = reaper.NamedCommandLookup("_XENAKIOS_SHUFFLESELECTTAKES")
+reaper.Main_OnCommand(actionNumber, 0) 
+
